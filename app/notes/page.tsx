7d2d518d40b8cb749854dc01243f9c0bb00da387
Antoine Favereau <93597506+antoinefavereau@users.noreply.github.com>
2025-01-30 17:28:21 +0000

    Implement notes management with mock database and routing

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,22 +1,31 @@
-"use client";
-
 import Link from "next/link";
-import { useState } from "react";
+import { createNoteServer, getAllNotesServer } from "../lib/mockDatabase";
 
-export default function Notes() {
-  const [notes, setNotes] = useState([]);
+export default async function Notes() {
+  const notes = await getAllNotesServer();
+
+  const defaultNote = {
+    id: notes.length + 1,
+    title: "Nouvelle note",
+    slug: "nouvelle-note",
+    content: "Contenu de la nouvelle note",
+  };
+
+  async function createNote() {
+    await createNoteServer(defaultNote);
+  }
 
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center gap-4">
       <h1 className="text-4xl">Notes</h1>
       <ul>
         {notes.map((note) => (
-          <li>
-            <Link></Link>
+          <li key={note.id}>
+            <Link href={`/notes/${note.slug}`}>{note.title}</Link>
           </li>
         ))}
         <li>
-            Nouvelle note
+          <button onClick={() => createNote()}>Nouvelle note</button>
         </li>
       </ul>
     </div>
